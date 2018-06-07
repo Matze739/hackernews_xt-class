f677e4aa329f3dbfb9008f37f59d8fe23bff0eb2
Marcel Wagner <mwagner2@sapient.com>
2018-06-07 16:14:12 +0200

    renamed file folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import NewsList from './messages/news-list';
+import NewsList from './news/news-list';
 
 class App extends Component {
   render(){
